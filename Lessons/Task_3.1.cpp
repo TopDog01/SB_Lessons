7f8bc2224ab2d3e59abfdaac11568cc7e7d6c8a4
Yuri Konev <crazy_clown93@mail.ru>
2024-12-10 19:17:51 +0300

    Task 1

---
+++ Lessons/Task_3.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	int a;
+	std::cout << "Enter number: ";
+	std::cin >> a;
+	int square = a * a;
+	std::cout << "square of the number " << a << " equals " << square << std::endl;
+	return 0;
+
+}
