0cb3e2acf5cd58d241064929e2c37e6151ef3e7b
Yuri Konev <crazy_clown93@mail.ru>
2024-12-19 00:07:49 +0300

    Tipe int

---
+++ Lessons/Task_3.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+int main() {
+//
+//	//int productCost = 6400;
+//
+//	//int deliveryCost = 350;
+//
+//	//int discount = 700;
+
+    setlocale(LC_ALL, "Russian");
+
+    int productCost;
+    std::cout << "¬ведите стоимость товара: ";
+    std::cin >> productCost;
+
+    int deliveryCost;
+    std::cout << "¬ведите стоимость доставки: ";
+    std::cin >> deliveryCost;
+
+    int discount;
+    std::cout << "¬ведите размер скидки: ";
+    std::cin >> discount;
+
+    int price = productCost + deliveryCost - discount;
+    std::cout << "---------";
+    std::cout << "»того: " << price << "\n";
+}
