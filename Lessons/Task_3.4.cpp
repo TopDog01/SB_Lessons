7f0088124849bbf9f3006bf7a418af5262413224
Yuri Konev <crazy_clown93@mail.ru>
2025-01-05 23:38:18 +0300

    3.4

---
+++ Lessons/Task_3.4.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//
+//int main() {
+//	setlocale(LC_ALL, "Russian");
+//	std::cout << "Давай копить!\n";
+//	std::string goal;
+//
+//	std::cout << "А на что? Введите цель: ";
+//	std::cin >> goal;
+//
+//	int total = 0;
+//
+//	int money;
+//	std::cout << "Сумма 1: ";
+//	std::cin >> money;
+//	total = total + money;
+//
+//	std::cout << "Сумма 2: ";
+//	std::cin >> money;
+//	total = total + money;
+//
+//	std::cout << "Сумма 3: ";
+//	std::cin >> money;
+//	total = total + money;
+//
+//	std::cout << "Сумма 4: ";
+//	std::cin >> money;
+//	total = total + money;
+//
+//	std::cout << "Сумма 5: ";
+//	std::cin >> money;
+//	total = total + money;
+//
+//	std::cout << "==========\n";
+//	std::cout << "Ты копил на " << goal << ". Получилось " << total << " руб.\n";
+//}
