9e03d24eb8e800fc9611599e931b6001c5fbde1d
Yuri Konev <crazy_clown93@mail.ru>
2025-01-06 01:30:29 +0300

    3.4.1

---
+++ Lessons/Task_3.4.1.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//
+//int main() {
+//	setlocale(LC_ALL, "Russian");
+//
+//	std::cout << "Бамбук VS Вредители!\n";
+//
+//	int startHeidght = 100;
+//	int growth = 50;
+//	int losses = 20;
+//
+//	/*int currentHeight = startHeidght;
+//
+//
+//
+//	currentHeight += growth;
+//	currentHeight -= losses;
+//
+//	currentHeight += growth;
+//	currentHeight -= losses;
+//
+//	currentHeight += growth / 2;*/
+//
+//	/*int thirdDayHeight = startHeidght + ((growth - losses) * 35 + (growth / 2));*/
+//
+//	int thirdDayGrowth = growth * 2 + growth / 2;
+//	int thirdDayLosses = losses * 2;
+//	int thirdDayHeight = startHeidght + thirdDayGrowth - thirdDayLosses;
+//
+//	std::cout << "Высота бамбука к середине 3 дня: " << thirdDayHeight << ".\n";
+//}
