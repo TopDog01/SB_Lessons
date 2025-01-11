05fb0b59d83d84a194e06f58475eafa58ad2acc4
Yuri Konev <crazy_clown93@mail.ru>
2025-01-11 22:14:52 +0300

    3

---
+++ Lessons/Task3.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//int main() {
+//    setlocale(LC_ALL, "Russian");
+//
+//    double x;
+//
+//    std::cout << "Введите число: ";
+//    std::cin >> x;
+//
+//    // Проверяем, является ли число отрицательным
+//    if (x < 0) {
+//        x = -x; // Обращаем знак числа
+//    }
+//
+//    std::cout << "Модуль числа: " << x << std::endl;
+//
+//    return 0;
+//}
