1700bf6c17796bb701f9949bb318c5de01062d8b
Yuri Konev <crazy_clown93@mail.ru>
2025-01-25 22:31:07 +0300

    1

---
+++ Lesson_5/Lesson_5/Lesson_5.cpp
@@ -0,0 +1,29 @@
+﻿//#include <iostream>
+//
+//int main() {
+//	setlocale(LC_ALL, "Russian");
+//
+//	int a;
+//	int b;
+//	int c;
+//	std::cout << "Введите три числа : \n";
+//	std::cin >> a >> b >> c;
+//
+//	if (a > b) {
+//		if (a > c) {
+//			std::cout << "Максимальное число равно " << a;
+//		}
+//		else {
+//			std::cout << "Максимальное число равно " << c;
+//		}
+//	}
+//	else {
+//		if (b > c) {
+//			std::cout << "Максимальное число равно " << b;
+//		}
+//		else {
+//			std::cout << "Максимальное число равно " << c;
+//		}
+//	}
+//}
+
