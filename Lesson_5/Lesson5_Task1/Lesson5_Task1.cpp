0bf48be72cde5a218759fda4a14fda0065b8b9b5
Yuri Konev <crazy_clown93@mail.ru>
2025-01-29 19:47:46 +0300

    2

---
+++ Lesson_5/Lesson5_Task1/Lesson5_Task1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double x, y;
+
+    // Ввод координат точки
+    std::cout << "Введите координаты точки (X Y): ";
+    std::cin >> x >> y;
+
+    // Определение положения точки
+    if (x == 0 || y == 0) {
+        std::cout << "Точка лежит на границе." << std::endl;
+    }
+    else if (x > 0 && y > 0) {
+        std::cout << "Точка находится в первой четверти." << std::endl;
+    }
+    else if (x < 0 && y > 0) {
+        std::cout << "Точка находится во второй четверти." << std::endl;
+    }
+    else if (x < 0 && y < 0) {
+        std::cout << "Точка находится в третьей четверти." << std::endl;
+    }
+    else if (x > 0 && y < 0) {
+        std::cout << "Точка находится в четвертой четверти." << std::endl;
+    }
+
+    return 0;
+}
