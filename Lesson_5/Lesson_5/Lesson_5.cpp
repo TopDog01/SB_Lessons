9630b36afd575ab2619f53787cfe5d379846327f
Yuri Konev <crazy_clown93@mail.ru>
2025-01-29 21:25:55 +0300

    L5.2

--- Lesson_5/Lesson_5/Lesson_5.cpp
+++ Lesson_5/Lesson_5/Lesson_5.cpp
@@ -1,29 +1,45 @@
-﻿//#include <iostream>
-//
-//int main() {
-//	setlocale(LC_ALL, "Russian");
-//
-//	int a;
-//	int b;
-//	int c;
-//	std::cout << "Введите три числа : \n";
-//	std::cin >> a >> b >> c;
-//
-//	if (a > b) {
-//		if (a > c) {
-//			std::cout << "Максимальное число равно " << a;
-//		}
-//		else {
-//			std::cout << "Максимальное число равно " << c;
-//		}
-//	}
-//	else {
-//		if (b > c) {
-//			std::cout << "Максимальное число равно " << b;
-//		}
-//		else {
-//			std::cout << "Максимальное число равно " << c;
-//		}
-//	}
-//}
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int a;
+	int b;
+	int c;
+	std::cout << "Введите три числа : \n";
+	std::cin >> a >> b >> c;
+
+	int max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+
+	std::cout << "Максимальное число равно " << max;
+
+	/*if (a >= b && a >= c) {
+		std::cout << "Максимальное число равно " << a;
+	}
+	else if (b >= a && b >= c) {
+		std::cout << "Максимальное число равно " << b;
+	}
+	else if (c >= a && c >= b) {
+		std::cout << "Максимальное число равно " << c;
+	}*/
+
+	/*if (a > b) {
+		if (a > c) {
+			std::cout << "Максимальное число равно " << a;
+		}
+		else {
+			std::cout << "Максимальное число равно " << c;
+		}
+	}
+	else {
+		if (b > c) {
+			std::cout << "Максимальное число равно " << b;
+		}
+		else {
+			std::cout << "Максимальное число равно " << c;
+		}
+	}*/
+}
 
