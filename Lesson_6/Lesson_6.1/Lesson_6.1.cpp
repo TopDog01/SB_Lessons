938029df943bfbae0475c90cbd3be95b57904d40
Yuri Konev <crazy_clown93@mail.ru>
2025-03-28 21:25:12 +0300

    new

---
+++ Lesson_6/Lesson_6.1/Lesson_6.1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    int count = 0;
+    while (count < 100) {
+        std::cout << "Через забор прыгнула овца.\n";
+        count += 1;
+    }
+    
+}
+
---
+++ Lesson_6/Lesson_6/Lesson_6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	std::string password;
+	std::cout << "Введите пароль:\n";
+	std::cin >> password;
+
+	while (password != "blablabla") {
+		std::cout << "Введите пароль\n";
+		std::cin >> password;
+	}
+	std::cout << "Правильный пароль\n";
+}
