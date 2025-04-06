b4fe07f606f63943eb7e0031909d632c655d8b9a
Yuri Konev <crazy_clown93@mail.ru>
2025-04-06 12:39:31 +0300

    6.2

---
+++ Lesson_6/Lesson_6.2/Lesson_6.2.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int count;
+	std::cout << "Сколько чисел вы хотите просуммировать?\n";
+	std::cin >> count;
+
+	std::cout << "Окей, введите " << count << "чисел:\n";
+
+	int iterations = 0;
+	int sum = 0;
+	while (iterations < count) {
+		int number;
+		std::cin >> number;
+		sum += number;
+		iterations += 1;
+	}
+	std::cout << "Сумма равна " << sum;
+
+}
