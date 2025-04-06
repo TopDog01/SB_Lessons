05729835a51f4c590ddb04b3dafe79802df58ba9
Yuri Konev <crazy_clown93@mail.ru>
2025-04-06 12:45:24 +0300

    6.3

---
+++ Lesson_6/Lesson_6.3/Lesson_6.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a;
+    std::cout << "Введите основание степени:\n";
+    std::cin >> a;
+
+    int b;
+    std::cout << "Введите показатель степени:\n";
+    std::cin >> b;
+
+    int iterations = 0;
+    int result = 1;
+    while (iterations < b) {
+        result *= a;
+        iterations += 1;
+    }
+
+    std::cout << "Результат равен " << result;
+}
