2710126f50a9ebae448c94c70dddc2801c76a340
Yuri Konev <crazy_clown93@mail.ru>
2025-04-06 12:53:36 +0300

    6.4

---
+++ Lesson_6/Lesson_6.4/Lesson_6.4.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int number;
+    std::cout << "Введите положительное число без нулей в начале:\n";
+    std::cin >> number;
+
+    int sum = 0;
+   
+    while (number != 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    std::cout << "Сумма цифр равна " << sum;
+}
