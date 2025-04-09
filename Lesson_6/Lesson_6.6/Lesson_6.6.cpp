11df67d6f4947cfc6811e40876136d3aaf676409
Yuri Konev <crazy_clown93@mail.ru>
2025-04-09 20:07:39 +0300

    6.6

---
+++ Lesson_6/Lesson_6.6/Lesson_6.6.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int number;
+    std::cout << "Введите положительное число\n";
+    std::cin >> number;
+
+    int current = 2;
+    bool found = false;
+    while (current  * current <= number && !found) {
+        found = number % current == 0;
+    }
+
+    if (!found) {
+        std::cout << "Это простое число";
+    }
+    else {
+        std::cout << "Это не просто число";
+    }
+
+}
+
