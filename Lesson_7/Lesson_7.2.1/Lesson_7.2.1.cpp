3ad5ca07673ab8d078af5dc338d3a624ff0e28ff
Yuri Konev <crazy_clown93@mail.ru>
2025-04-15 16:25:02 +0300

    7.2.1

---
+++ Lesson_7/Lesson_7.2.1/Lesson_7.2.1.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int totalMonths;
+    std::cout << "Анатолий, как долго собираетесь копить? ";
+    std::cin >> totalMonths;
+
+    int savedMoney = 0;
+    int sum = 0;
+
+    for (int month = 0; month < totalMonths; month++)
+    {
+        std::cout << "Месяц" << month << " ";
+        std::cout << "Сколько ты хочешь отложить рублей? ";
+        std::cin >> savedMoney;
+        sum += savedMoney;
+    }
+
+    std::cout << "Поздравляем, за " << totalMonths << " ты накопишь " << sum << " рублей!\n";
+}
+
