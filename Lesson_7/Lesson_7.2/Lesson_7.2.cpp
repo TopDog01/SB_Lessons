92c515b4a290914d258b0dc12ab1d585deb930cd
Yuri Konev <crazy_clown93@mail.ru>
2025-04-15 16:06:22 +0300

    7.2

---
+++ Lesson_7/Lesson_7.2/Lesson_7.2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ;
+
+	for (int counter = 0; counter < 100; counter++)
+	{
+		std::cout << "Через забор перепрыгнула овца " << counter << "\n";
+	}
+}
