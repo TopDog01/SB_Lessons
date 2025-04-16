d88f4a15edcfd4df1a405daf192a88f06fe7805c
Yuri Konev <crazy_clown93@mail.ru>
2025-04-16 14:08:11 +0300

    7.3

---
+++ Lesson_7/Lesson_7.3/Lesson_7.3.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Какого размера ваше письмо? ";
+    int startSize;
+    std::cin >> startSize;
+
+    int timesFolded = 0;
+
+
+    for (int size = startSize; size > 12; size /= 2) {
+        timesFolded += 2;
+    }
+
+    std::cout << "Вам нужно сложить письмо " << timesFolded << " раз.";
+}
