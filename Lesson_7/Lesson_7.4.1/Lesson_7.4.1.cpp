15ecdfd4a13888af32df857962ab934166072fe0
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 08:11:47 +0300

    7.4.1

---
+++ Lesson_7/Lesson_7.4.1/Lesson_7.4.1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main()
+{
+   
+	for (int a = 1; a <= 9; a++)
+	{
+		for (int b = 1; b <= 9; b++)
+		{
+			std::cout << a * b << "\t";
+		}
+		std::cout << "\n";
+	}
+	
+}
+
