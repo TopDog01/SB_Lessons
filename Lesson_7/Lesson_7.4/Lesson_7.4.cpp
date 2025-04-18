c10a3bb7752164a1ba9b7547459da98c4a51bf22
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 08:02:12 +0300

    7.4

---
+++ Lesson_7/Lesson_7.4/Lesson_7.4.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    while (true) {
+        std::cout << "Следующий клиент, Добро пожаловать :)\n";
+        std::string pinCode;
+
+        for (int tries = 0; tries < 3; tries++) {
+            std::cout << "Введите пин-код: ";
+            std::cin >> pinCode;
+
+            if (pinCode == "4224") {
+                std::cout << "Спасибо, вот ваша зарплата...\n";
+                break;
+            }
+            else {
+                std::cout << "Неверный пин-код :(\n";
+            }
+        }
+
+        std::cout << "Всего доброго!\n";
+    }
+
+    
+}
