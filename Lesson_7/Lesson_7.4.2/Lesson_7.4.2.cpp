a58428d137d6a482139827d1e713039d0c99b1df
Yuri Konev <crazy_clown93@mail.ru>
2025-04-18 08:20:04 +0300

    7.4.2

---
+++ Lesson_7/Lesson_7.4.2/Lesson_7.4.2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    for (int row = 0; row <= 20; row++) {
+        for (int col = 0; col <= 50; col++) {
+            if (row == 10)
+                std::cout << "-";
+            else if (col == 25)
+                std::cout << "|";
+            else if (col == -row + 20)
+                std::cout << "/";
+            else if (col == row + 30)
+                std::cout << "\\";
+            else
+                std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
