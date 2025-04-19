cf2cb33c033ec288157162467d8a3f3e6a9cd673
Yuri Konev <crazy_clown93@mail.ru>
2025-04-19 22:09:24 +0300

    8.2

---
+++ Lesson_8/Lesson_8.2/Lesson_8.2.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+
+    float distance = 3, time;
+
+    std::cout << "За сколько секунд автомобиль проехал отрзок " << distance << " км? ";
+    std::cin >> time;
+
+    float meanSpeed = distance / time;
+    meanSpeed *= 3600.0f;
+
+    std::cout << "Средняя скорость автомобиля: " << meanSpeed << "\n";
+
+    if (meanSpeed > 80.0f)
+    {
+        std::cout << "Выписан штраф!";
+    }
+
+}
