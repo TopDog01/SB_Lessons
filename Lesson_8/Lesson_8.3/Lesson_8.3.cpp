4de90ed09f83dc6b7cef4309bb543317582ee46f
Yuri Konev <crazy_clown93@mail.ru>
2025-04-19 22:31:44 +0300

    1

---
+++ Lesson_8/Lesson_8.3/Lesson_8.3.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    float force;
+
+    while (true) {
+        std::cout << "Сила удара: ";
+        std::cin >> force;
+        force *= 10;
+        std::cout << "Балл: " <<  (int) force << "\n";
+    }
+}
+
