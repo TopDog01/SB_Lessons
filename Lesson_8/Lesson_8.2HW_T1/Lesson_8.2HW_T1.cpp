7cba3d3fec32e4c6d5697ce9d182678a0865042c
Yuri Konev <crazy_clown93@mail.ru>
2025-04-19 22:15:31 +0300

    T1

---
+++ Lesson_8/Lesson_8.2HW_T1/Lesson_8.2HW_T1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    float price_per_100g; // цена за 100 грамм
+    float weight;         // вес покупки
+    float total_cost;    // итоговая стоимость
+
+    // Запрашиваем у пользователя цену за 100 грамм
+    cout << "Введите цену товара за 100 грамм (в рублях): ";
+    cin >> price_per_100g;
+
+    // Запрашиваем у пользователя вес покупки
+    cout << "Введите вес покупки (в граммах): ";
+    cin >> weight;
+
+    // Вычисляем итоговую стоимость
+    total_cost = (price_per_100g / 100) * weight;
+
+    // Выводим итоговую стоимость
+    cout << "Итоговая стоимость покупки: " << total_cost << " рублей." << endl;
+
+    return 0;
+}
