5dde4f94e2225c8a7e21adca1d68e2a4d6c614cf
Yuri Konev <crazy_clown93@mail.ru>
2025-04-20 21:23:33 +0300

    ////

---
+++ Lesson_8/Lesson_8.4/Lesson_8.4.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    
+    setlocale(LC_ALL, "Russian");
+    
+    float x, y;
+    std::cout << "Введите координату х: ";
+    std::cin >> x;
+    std::cout << "Введите координату y: ";
+    std::cin >> y;
+
+
+    float distance = std::sqrt(x * x + y * y);
+
+
+    std::cout << "Расстояние от вас до точки, равно " << distance << "\n";
+
+}
+
