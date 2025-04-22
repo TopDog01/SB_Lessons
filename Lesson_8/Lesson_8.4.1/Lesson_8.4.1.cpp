a9805f643d01ce139b579a6a29e27957842ab110
Yuri Konev <crazy_clown93@mail.ru>
2025-04-22 21:51:02 +0300

    /////

---
+++ Lesson_8/Lesson_8.4.1/Lesson_8.4.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	float distance, angle;
+	std::cout << "Введите расстояние до танка: ";
+	std::cin >> distance;
+	std::cout << "Введите угол: ";
+	std::cin >> angle;
+	angle /= 57.296;
+
+	float x, y;
+	x = std::cos(angle) * distance;
+	y = std::sin(angle) * distance;
+
+	std::cout << "Координаты вражеского танка: ";
+	std::cout << x << ", " << y << "\n";
+}
