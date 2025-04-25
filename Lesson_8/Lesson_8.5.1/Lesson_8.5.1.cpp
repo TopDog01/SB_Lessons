3e6b50de4765fecb3644a19b8ee9f3a35751124b
Yuri Konev <crazy_clown93@mail.ru>
2025-04-25 21:22:44 +0300

    80000

---
+++ Lesson_8/Lesson_8.5.1/Lesson_8.5.1.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    float a = 80000.f;
+    int i = 0;
+    while (true) {
+        a += 0.001f;
+        i++;
+        std::cout << "Итерация " << i << ": " << a << "\n";
+    }
+}
+
+
