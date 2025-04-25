b525d64eb466271f8b4efc7847ab8d475ce76d1e
Yuri Konev <crazy_clown93@mail.ru>
2025-04-25 21:12:39 +0300

    квадратный корень из числа с точностью

---
+++ Lesson_8/Leeson_8.5/Leeson_8.5.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+
+    float number, precision;
+    std::cout << "Из какого числа извлекать квадратный корень? ";
+    std::cin >> number;
+    std::cout << "Укажите желаемую точность вычислений: ";
+    std::cin >> precision;
+
+    float from = 0.0f;
+    float to = number, resultPrecision;
+    float result;
+
+    do
+    {
+        result = from + (to - from) / 2.0f;
+        float sqrResult = result * result;
+        if (sqrResult < number)
+        {
+            from = result;
+        }
+        else
+        {
+            to = result;
+        }
+        resultPrecision = std::fabs(sqrResult - number);
+        std::cout << resultPrecision << "\n";
+    } while (resultPrecision > precision);
+
+    std::cout << "Квадратный корень из " << number << " равен " << result << "\n";
+}
+
