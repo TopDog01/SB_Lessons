c52b879a4352f59ce3215cdff1e229edf165d483
Yuri Konev <crazy_clown93@mail.ru>
2025-04-28 20:26:47 +0300

    разбор задания

---
+++ Lesson_9/Lesson_9.1/Lesson_9.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	float start;
+	float finish;
+	std::cout << "Введите начальную и конечную амплитуду:\n";
+	std::cin >> start >> finish;
+
+	while (start < 0 || finish < 0 || finish > start) {
+		std::cout << "Некорректный ввод! Введите еще раз:\n";
+		std::cin >> start >> finish;
+	}
+
+	int count = 0;
+	while (start > finish) {
+		start *= 1 - 0.084f;
+		count++;
+	}
+
+	std::cout << "Маятник достигнет нужной амплитуды через " << count << " колебаний\n";
+}
