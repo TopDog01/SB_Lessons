3ec1915d4f2842f330ed8f798bd46131fb4c96fd
Yuri Konev <crazy_clown93@mail.ru>
2025-04-28 21:51:26 +0300

    simbols

---
+++ Lesson_9/Lesson_9.3/Lesson_9.3.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите координаты:\n";
+    char x, y;
+    std::cin >> x >> y;
+
+    if (((x >= 'A' && x <= 'H') || (x >= 'a' && x <= 'h')) && y >= '1' && y <= '8') {
+        std::cout << "Yes\n";
+    }
+    else {
+        std::cout << "No!\n";
+    }
+
+}
