a73cb4b50c10ad737e186ceb99618cada85ee36d
Yuri Konev <crazy_clown93@mail.ru>
2025-05-04 20:11:14 +0300

    char

---
+++ Lesson_9/Lesson_9.3.1/Lesson_9.3.1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::cout << "Input the time in HH:MM format:\n";
+    std::string time;
+    std::cin >> time;
+
+    if (time.length() != 5 || time[2] != ':' || time[0] < '0'
+        || time[0] > '9' || time[1] < '0' || time[1] > '9'
+        || time[3] < '0' || time[3] > '9' || time[4] < '0'
+        || time[4] > '9') {
+        std::cout << "Error!\n";
+    }
+    else {
+        int hours = 10 * (time[0] - '0') + (time[1] - '0');
+        int minutes = 10 * (time[3] - '0') + (time[4] - '0');
+
+        if (hours > 23 || minutes > 59) {
+            std::cout << "Error!\n";
+        }
+        else {
+        std::cout << 60 * hours + minutes;
+        }
+        
+
+    }
+
+
+}
