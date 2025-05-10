4bcca3affe4f2c5982704fd9d61d989f4706f769
Yuri Konev <crazy_clown93@mail.ru>
2025-05-10 13:54:04 +0300

    task 1, 2

---
+++ Lesson_9/Lesson_9.4/Lesson_9.4.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::string str;
+    std::cout << "Input string:\n";
+    std::cin >> str;
+
+    bool good = true;
+
+    for (int i = 0; i < str.length() && good; i++)
+    {
+        if (str[i] < 'a' || str[i] > 'z') {
+            good = false;
+            
+        }
+            
+    }
+    for (int i = 0; i + 1 < str.length() && good; i++)
+    {
+        if (str[i] > str[i + 1]) {
+            good = false;
+            
+        }
+
+    }
+
+    if (good) {
+        std::cout << "Yes!\n";
+    }
+    else {
+        std::cout << "No!\n";
+    }
+}
+
