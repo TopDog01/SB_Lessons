752a56dcd17d550d8fc45b37dd1a68d373010628
Yuri Konev <crazy_clown93@mail.ru>
2025-05-26 20:05:44 +0300

    замена строк

---
+++ Lesson_9/Lesson_9.6.1/Lesson_9.6.1.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::string text;
+    std::cout << "Input text:\n";
+    std::cin >> text;
+
+    std::string word = "Voldemort";
+    std::string newword = "You-Know-Who";
+
+    std::string result = " ";
+
+    bool found = false;
+    for (int startPos = 0; startPos < text.length(); startPos++)
+    {
+        bool match = true;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (i + startPos >= text.length() || word[i] != text[i + startPos]) {
+                match = false;
+                break;
+            }
+        }
+        if (!match)
+        {
+            result += text[startPos];
+            
+        }
+        else
+        {
+            result += newword;
+            startPos += word.length() - 1;
+        }
+    }
+    std::cout << result << '\n';
+}
+
