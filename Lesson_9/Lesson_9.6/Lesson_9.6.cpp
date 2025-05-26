492d3b746623d3e9e5b98379dd6d1b496d9abf69
Yuri Konev <crazy_clown93@mail.ru>
2025-05-26 18:29:35 +0300

    task 1

---
+++ Lesson_9/Lesson_9.6/Lesson_9.6.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::string text;
+    std::string word;
+    std::cout << "Input the text and the word:\n";
+    std::cin >> text >> word;
+
+
+    bool found = false;
+    for (int startPos = 0; startPos + word.length() <= text.length(); startPos++)
+    {
+        bool match = true;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (word[i] != text[i + startPos])
+            {
+                match = false;
+                break;
+            }
+        }
+        if (match)
+        {
+            found = true;
+            break;
+        }
+    }
+
+    
+
+    if (found)
+    {
+        std::cout << "Yes!\n";
+    }
+    else {
+        std::cout << "No!\n";
+    }
+}
+
