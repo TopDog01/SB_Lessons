c6100d9cdd95ada8b47f4515447aba2e94afcc48
Yuri Konev <crazy_clown93@mail.ru>
2025-05-26 15:19:46 +0300

    сравнение символов

---
+++ Lesson_9/Lesson_9.4.1/Lesson_9.4.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+
+	std::string str;
+	std::cout << "Input string:\n";
+	std::cin >> str;
+
+	bool palindrome = true;
+	int lastIndex = str.length() - 1;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] != str[lastIndex - i]) {
+			palindrome = false;
+			break;
+		}
+	}
+
+	if (palindrome) {
+		std::cout << "Yes!\n";
+	}
+	else
+	{
+		std::cout << "No!\n";
+	}
+}
