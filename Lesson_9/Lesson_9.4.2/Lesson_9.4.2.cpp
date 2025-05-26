6d2066f54a18ed8ab8226bc39ab6b865855623e1
Yuri Konev <crazy_clown93@mail.ru>
2025-05-26 16:07:47 +0300

    сравнение знаков

---
+++ Lesson_9/Lesson_9.4.2/Lesson_9.4.2.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::string first;
+    std::string second;
+    std::cout << "Input two strings:\n";
+    std::cin >> first >> second;
+
+    if (first < second)
+    {
+        std::cout << "Yes!\n";
+    }
+    else {
+        std::cout << "No!\n";
+    }
+}
+
