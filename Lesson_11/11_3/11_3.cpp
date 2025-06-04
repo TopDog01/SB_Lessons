d30969205d53f024a8ca765d9781928c59764008
Yuri Konev <crazy_clown93@mail.ru>
2025-06-04 12:56:10 +0300

    11.3

---
+++ Lesson_11/11_3/11_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+bool is_prime(int n) {
+	if (n == 1) return false;
+	for (int d = 2; d * d <= n; d++)
+	{
+		if (n % d == 0) {
+			return false;
+			
+		}
+	}
+	return true;
+}
+int main() {
+	int a, b;
+	std::cout << "Rande: " << std::endl;
+	std::cin >> a >> b;
+
+	int count = 0;
+	for (int i = a; i <= b; i++)
+	{
+		
+		if (is_prime(i)) {
+			count++;
+		}
+	}
+
+
+	std::cout << "Count:" << count << std::endl;
+}
